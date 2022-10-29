146ba6c66b6794a38e45b854f699db9c297cd111
Laura Varga <lvarga@guidewire.com>
2022-10-29 19:16:45 +0100

    finalizing day3 solution

--- 2021-2/day3/day3.go
+++ 2021-2/day3/day3.go
@@ -6,10 +6,83 @@ import (
 	"log"
 	"math"
 	"os"
+	"strconv"
 )
+
+func comps(readings []string, cursor int) (int, int) {
+	zs := 0
+	ons := 0
+	for i := 0; i < len(readings); i++ {
+		if string(readings[i][cursor]) == "0" {
+			zs++
+		} else {
+			ons++
+		}
+	}
+	return zs, ons
+}
+
+func o2gen(readings []string, cursor int) string {
+	fmt.Printf("recursive: %v \n", len(readings))
+	if len(readings) == 1 {
+		return readings[0]
+	}
+	if cursor > len(readings[0]) {
+		return ""
+	}
+	numbers := make([]string, 0)
+	zs, ons := comps(readings, cursor)
+	if zs > ons {
+		for i := 0; i < len(readings); i++ {
+			if string(readings[i][cursor]) == "0" {
+				numbers = append(numbers, readings[i])
+			}
+		}
+		cursor += 1
+		return o2gen(numbers, cursor)
+	} else {
+		for i := 0; i < len(readings); i++ {
+			if string(readings[i][cursor]) == "1" {
+				numbers = append(numbers, readings[i])
+			}
+		}
+		cursor += 1
+		return o2gen(numbers, cursor)
+	}
+}
+
+func co2gen(readings []string, cursor int) string {
+	fmt.Printf("recursive: %v \n", len(readings))
+	if len(readings) == 1 {
+		return readings[0]
+	}
+	if cursor > len(readings[0]) {
+		return ""
+	}
+	numbers := make([]string, 0)
+	zs, ons := comps(readings, cursor)
+	if zs <= ons {
+		for i := 0; i < len(readings); i++ {
+			if string(readings[i][cursor]) == "0" {
+				numbers = append(numbers, readings[i])
+			}
+		}
+		cursor += 1
+		return co2gen(numbers, cursor)
+	} else {
+		for i := 0; i < len(readings); i++ {
+			if string(readings[i][cursor]) == "1" {
+				numbers = append(numbers, readings[i])
+			}
+		}
+		cursor += 1
+		return co2gen(numbers, cursor)
+	}
+}
 
 func main() {
 	fmt.Println("day3 2021")
+	// TODO: rework to not have static array
 	zeroes := [12]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	ones := [12]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	f, err := os.Open("in.txt")
@@ -17,8 +90,10 @@ func main() {
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(f)
+	var readings []string
 	for scanner.Scan() {
 		bline := scanner.Text()
+		readings = append(readings, bline)
 		for pos, char := range bline {
 			if string(char) == "0" {
 				zeroes[pos] += 1
@@ -28,7 +103,7 @@ func main() {
 		}
 	}
 	gamma, epsilon := 0, 0
-	for i := 11; i >= 0; i-- {
+	for i := len(zeroes) - 1; i >= 0; i-- {
 		if zeroes[i] > ones[i] {
 			epsilon += int(math.Pow(2, float64(11-i)))
 		} else {
@@ -36,5 +111,19 @@ func main() {
 		}
 	}
 	r := epsilon * gamma
-	fmt.Printf("result is: %v \n", r)
+	fmt.Printf("power consumption is: %v \n", r)
+	o2g := o2gen(readings, 0)
+	co2g := co2gen(readings, 0)
+	o2gdec, err := strconv.ParseInt(o2g, 2, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	co2gdec, err := strconv.ParseInt(co2g, 2, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("o2 generator is: %v \n", o2gdec)
+	fmt.Printf("Co2 generator is: %v \n", co2gdec)
+	fmt.Printf("co2 scrubber value: %v \n", o2gdec*co2gdec)
+
 }
