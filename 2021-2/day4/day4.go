a3d0784184653be3a4daa5d0e3c1d091e66361d2
Laura Varga <lvarga@guidewire.com>
2022-12-02 10:37:33 +0000

    cleanup.. no idea

--- 2021-2/day4/day4.go
+++ 2021-2/day4/day4.go
@@ -9,20 +9,17 @@ import (
 	"strings"
 )
 
-var allGrids [][5][5]map[int]bool
+var allGrids [][5]map[int]bool
 
-// func lineToBLine(line string) map[int]bool {
-// 	bLine := make(map[int]bool, 5)
-// 	xLine := strings.Split(line, ",")
-// 	for i := 0; i < len(xLine); i++ {
-// 		num, err := strconv.Atoi(xLine[i])
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		bLine[num] = false
-// 	}
-// 	return bLine
-// }
+func mark(extr int) {
+	for i := 0; i < 5; i++ {
+		for k, _ := range grid[i] {
+			if k == extr {
+				grid[i][k] = true
+			}
+		}
+	}
+}
 
 func main() {
 	fmt.Println("welcome to day4!")
@@ -32,12 +29,11 @@ func main() {
 	}
 
 	scanner := bufio.NewScanner(f)
+	extrNumbers := make([]int, 0)
 	if scanner.Scan() {
 		extraction := strings.Split(scanner.Text(), ",")
 		fmt.Printf("extractions: %v \n", extraction)
 		n := len(extraction)
-		// var extrNumbers []int
-		extrNumbers := make([]int, 0)
 		for i := 0; i < n; i++ {
 			num, err := strconv.Atoi(extraction[i])
 			if err != nil {
@@ -46,22 +42,20 @@ func main() {
 			extrNumbers = append(extrNumbers, num)
 		}
 	}
-	var bingoGrid [5][5]map[int]bool
+	var bingoGrid [5]map[int]bool
 	i := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line != "" {
 			fmt.Printf("full line: %v \n", line)
-			// bLine := lineToBLine(line)
-			// bingoGrid[i] = append(bingoGrid[i], bLine)
 			xLine := strings.Fields(line)
+			bingoGrid[i] = make(map[int]bool, 5)
 			for j := 0; j < len(xLine); j++ {
 				num, err := strconv.Atoi(xLine[j])
 				if err != nil {
 					log.Fatal(err)
 				}
-				bingoGrid[i][j] = make(map[int]bool, 0)
-				bingoGrid[i][j][num] = false
+				bingoGrid[i][num] = false
 			}
 			i++
 		} else {
@@ -74,5 +68,14 @@ func main() {
 		}
 	}
 	allGrids = append(allGrids, bingoGrid)
-	fmt.Println("it's done!")
+	fmt.Println("let's play!")
+	// extracting number
+	for i := 0; i < len(extrNumbers); i++ {
+		//marking each grid & searching for winner (iterating through the grids)
+		for j := 0; j < len(allGrids); j++ {
+			if mark(allGrids[j], extrNumbers(i)) {
+				fmt.Printf("grid %v wins!", j)
+			}
+		}
+	}
 }
