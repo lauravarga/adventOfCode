642e9903ef1e8eac2c40237be340f604f161a2df
Laura Varga <lvarga@Lauras-MacBook-Air.local>
2022-12-04 17:29:45 +0000

    second star, second day. some refactoring, still have duplication, but that's ok

--- 2022/day2/day2.go
+++ 2022/day2/day2.go
@@ -14,6 +14,82 @@ type rockPaperScissors struct {
 	points   int
 }
 
+func option1(opponent string, advice string) int {
+	points := 0
+	if advice == "X" {
+		points += 1
+		switch opponent {
+		case "A":
+			points += 3
+		case "B":
+			points += 0
+		case "C":
+			points += 6
+		}
+	}
+	if advice == "Y" {
+		points += 2
+		switch opponent {
+		case "A":
+			points += 6
+		case "B":
+			points += 3
+		case "C":
+			points += 0
+		}
+	}
+	if advice == "Z" {
+		points += 3
+		switch opponent {
+		case "A":
+			points += 0
+		case "B":
+			points += 6
+		case "C":
+			points += 3
+		}
+	}
+	return points
+}
+
+func option2(opponent string, advice string) int {
+	points := 0
+	if advice == "X" {
+		points += 0
+		switch opponent {
+		case "A":
+			points += 3
+		case "B":
+			points += 1
+		case "C":
+			points += 2
+		}
+	}
+	if advice == "Y" {
+		points += 3
+		switch opponent {
+		case "A":
+			points += 1
+		case "B":
+			points += 2
+		case "C":
+			points += 3
+		}
+	}
+	if advice == "Z" {
+		points += 6
+		switch opponent {
+		case "A":
+			points += 2
+		case "B":
+			points += 3
+		case "C":
+			points += 1
+		}
+	}
+	return points
+}
+
 func main() {
 	var theGuide []rockPaperScissors
 	var instruction rockPaperScissors
@@ -24,48 +100,19 @@ func main() {
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(f)
-	sum := 0
+	sum1 := 0
+	sum2 := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		moves := strings.Split(line, "")
 		instruction.opponent = moves[0]
 		instruction.advice = moves[2]
-		instruction.points = 0
-		if instruction.advice == "X" {
-			instruction.points += 1
-			switch instruction.opponent {
-			case "A":
-				instruction.points += 3
-			case "B":
-				instruction.points += 0
-			case "C":
-				instruction.points += 6
-			}
-		}
-		if instruction.advice == "Y" {
-			instruction.points += 2
-			switch instruction.opponent {
-			case "A":
-				instruction.points += 6
-			case "B":
-				instruction.points += 3
-			case "C":
-				instruction.points += 0
-			}
-		}
-		if instruction.advice == "Z" {
-			instruction.points += 3
-			switch instruction.opponent {
-			case "A":
-				instruction.points += 0
-			case "B":
-				instruction.points += 6
-			case "C":
-				instruction.points += 3
-			}
-		}
-		sum += instruction.points
+		instruction.points = option1(instruction.opponent, instruction.advice)
+
+		sum1 += instruction.points
+		sum2 += option2(instruction.opponent, instruction.advice)
 		theGuide = append(theGuide, instruction)
 	}
-	fmt.Printf("the result is: %v\n", sum)
+	fmt.Printf("the first result is: %v\n", sum1)
+	fmt.Printf("the second result is: %v\n", sum2)
 }
