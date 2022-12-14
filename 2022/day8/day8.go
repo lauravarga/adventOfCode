8b75ce52f0b238180d7af394e40b04c3b0de287b
Laura Varga <lolovarga@gmail.com>
2022-12-14 16:46:51 +0000

    day 8, second star completed

--- 2022/day8/day8.go
+++ 2022/day8/day8.go
@@ -11,7 +11,7 @@ import (
 func isVisible(i int, j int, forrest [][]int) bool {
 	visible := true
 	for x := 0; x < i; x += 1 {
-		if (forrest[x][j] >= forrest[i][j]) && (x != i) {
+		if forrest[x][j] >= forrest[i][j] {
 			visible = false
 
 			break
@@ -20,7 +20,7 @@ func isVisible(i int, j int, forrest [][]int) bool {
 	if !visible {
 		visible = true
 		for x := i + 1; x < len(forrest[i]); x += 1 {
-			if (forrest[x][j] >= forrest[i][j]) && (x != i) {
+			if forrest[x][j] >= forrest[i][j] {
 				visible = false
 
 				break
@@ -30,7 +30,7 @@ func isVisible(i int, j int, forrest [][]int) bool {
 	if !visible {
 		visible = true
 		for y := 0; y < j; y += 1 {
-			if (forrest[i][y] >= forrest[i][j]) && (y != j) {
+			if forrest[i][y] >= forrest[i][j] {
 				visible = false
 
 				break
@@ -40,7 +40,7 @@ func isVisible(i int, j int, forrest [][]int) bool {
 	if !visible {
 		visible = true
 		for y := j + 1; y < len(forrest[i]); y += 1 {
-			if (forrest[i][y] >= forrest[i][j]) && (y != j) {
+			if forrest[i][y] >= forrest[i][j] {
 				visible = false
 
 				break
@@ -65,6 +65,68 @@ func visibleTrees(forrest [][]int, count int) int {
 		}
 	}
 	return count
+}
+
+func scenicScore(forrest [][]int, i int, j int) int {
+	score := 1
+	count := 0
+	for x := i - 1; x >= 0; x -= 1 {
+		if forrest[x][j] >= forrest[i][j] {
+			count += 1
+			break
+		}
+		count += 1
+	}
+	score = score * count
+	count = 0
+	for x := i + 1; x < len(forrest[i]); x += 1 {
+		if forrest[x][j] >= forrest[i][j] {
+			count += 1
+			break
+		}
+		count += 1
+	}
+	score = score * count
+	count = 0
+	for y := j - 1; y >= 0; y -= 1 {
+		if forrest[i][y] >= forrest[i][j] {
+			count += 1
+			break
+		}
+		count += 1
+	}
+	score = score * count
+	count = 0
+	for y := j + 1; y < len(forrest[i]); y += 1 {
+		if forrest[i][y] >= forrest[i][j] {
+			count += 1
+			break
+		}
+		count += 1
+	}
+	score = score * count
+	return score
+}
+
+func maxScenicScore(forrest [][]int) int {
+	max := 0
+	sscenicScore := 0
+	for i := 0; i < len(forrest); i += 1 {
+		for j := 0; j < len(forrest[i]); j += 1 {
+			if (i == 0) || (i == len(forrest)-1) {
+				sscenicScore = 0
+			} else if (j == 0) || (j == len(forrest[i])-1) {
+				sscenicScore = 0
+			} else {
+				sscenicScore = scenicScore(forrest, i, j)
+			}
+
+			if max < sscenicScore {
+				max = sscenicScore
+			}
+		}
+	}
+	return max
 }
 
 func main() {
@@ -94,5 +156,7 @@ func main() {
 	}
 	visibleTrees := visibleTrees(forrest, 0)
 	fmt.Printf("visible tree count: %v\n", visibleTrees)
+	maxScore := maxScenicScore(forrest)
+	fmt.Printf("max scenic score: %v\n", maxScore)
 	fmt.Println("it's done")
 }
